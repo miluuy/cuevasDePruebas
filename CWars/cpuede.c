556bc34e6f6339fddcadb2356c7b4d9b5f413512
miluuy <mtietirey@frba.utn.edu.ar>
2026-03-13 16:00:56 -0300

    introducción muuuy básica de c

---
+++ CWars/cpuede.c
@@ -0,0 +1,23 @@
+int main(void){ // queRetorna nombreFuncion (parametrosQueRecibe)
+    return 0;
+}
+
+/*
+en la terminal:
+para compilar: gcc cpuede.c -o nombreDelPrograma
+
+y ejecutar: ./nombreDelPrograma
+el punto de ./ referencia a la ruta en donde estamos ubicados
+si estamos en /casita/carpetita : /casita/carpetita/nombreDelPrograma = ./nombreDelPrograma
+
+con  echo $?  puedo saber cuál fue el valor de retorno
+
+para declarar variable: especificar su tipo de data seguido de su nombre
+operador de asignación es =
+
+
+" Las variables de C son meras referencias a algún bloque de memoria,
+siendo el tipo de la variable el que anuncia qué tamaño tiene esa referencia."
+La cantidad de bytes a reservar depende del tipo de dato que le declaramos a la variable
+en un int ocupa 4 bytes
+*/
